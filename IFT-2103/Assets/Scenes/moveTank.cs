3198e94b783af05176e2e600fa63c292c00bb34c
farmville3 <stlaurent_18@hotmail.com>
2018-10-09 18:33:54 -0400

    Init of the scene we'll be working on.Physics need to be implemented.

---
+++ IFT-2103/Assets/Scenes/moveTank.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class moveTank : MonoBehaviour {
+
+    float forwardRate = 15;
+    float turnRate = 3;
+
+	// Use this for initialization
+	void Start () {}
+	
+	// Update is called once per frame
+	void Update () {
+
+        //tank forward speed in action
+        float forwardMoveAmount = Input.GetAxis("Vertical") * forwardRate;
+
+        //force of the tank's turn
+        float turnForce = Input.GetAxis("Horizontal") * turnRate;
+
+        //rotate tank in action
+        transform.Rotate(0, turnForce, 0);
+
+        //Move tank
+        transform.position += transform.forward * forwardMoveAmount * Time.deltaTime;
+
+
+		
+	}
+}
---
+++ IFT-2103/Assets/Scenes/rotateTank.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class rotateTank : MonoBehaviour
+{
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey("z"))
+        {
+            transform.Rotate(0, -5, 0);
+        }
+        else if (Input.GetKey("c"))
+        {
+            transform.Rotate(0, 5, 0);
+        }
+    }
+}

46683df99fedd8289ace94c53a029c58226980ca
farmville3 <stlaurent_18@hotmail.com>
2018-10-12 23:32:27 -0400

    Initial commit

---
+++ IFT-2103/Assets/Scripts/bulletTank.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class bulletTank : MonoBehaviour
+{
+    protected Vector3 m_BulletAcceleration;
+
+    private Vector3 m_BulletVelocity;
+
+    void Update()
+    {
+        //New position
+        transform.position += m_BulletVelocity + 0.5f * m_BulletAcceleration * Time.deltaTime * Time.deltaTime;
+        //New velocity
+        m_BulletVelocity += m_BulletAcceleration * Time.deltaTime;
+    }
+
+    public void shootBullet(Vector3 p_BulletVelocity, Vector3 p_BulletAcceleration, Vector3 p_StartPosition, Quaternion p_StartRotation)
+    {
+        m_BulletVelocity = p_BulletVelocity;
+        m_BulletAcceleration = p_BulletAcceleration;
+        transform.position = p_StartPosition;
+        //transform.rotation = p_StartRotation;
+    }
+
+    private void bulletHitAWall()
+    {
+        //Code qui gère la colision entre un balle et un mur / un autre gameObject physique.
+    }
+}
---
+++ IFT-2103/Assets/Scripts/canonTank.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+public class canonTank : MonoBehaviour {
+
+    [SerializeField]
+    protected float m_Acceleration;
+    [SerializeField]
+    protected float m_Speed;
+    [SerializeField]
+    protected GameObject m_BulletPrefab;
+
+    protected Vector3 m_ShotAcceleration;
+
+    private Vector3 m_ShotVelocity;
+
+    private void Start()
+    {
+        m_ShotVelocity = new Vector3(m_Speed, 0f, 0f);
+        m_ShotAcceleration = new Vector3(m_Acceleration, 0f, 0f);
+    }
+
+    void Update () {
+        if (Input.GetButtonDown("Jump"))
+        {
+            Fire();    
+        }
+    }
+
+    public void Fire()
+    {
+        bulletTank bulletObject = Instantiate(m_BulletPrefab).GetComponent<bulletTank>();
+        bulletObject.transform.SetParent(null);
+        bulletObject.name = m_BulletPrefab.name;
+        bulletObject.shootBullet(m_ShotVelocity, m_ShotAcceleration, transform.position, transform.rotation);
+    }
+}

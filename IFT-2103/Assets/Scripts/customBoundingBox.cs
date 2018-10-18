e3e269b97f97d165ba040061ff4e9a73efae8f70
Guillaume Ouellet <guillaume.ouellet6@ulaval.ca>
2018-10-17 22:16:53 -0400

    Tank-walls collision.

---
+++ IFT-2103/Assets/Scripts/customBoundingBox.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+
+
+public class customBoundingBox : MonoBehaviour
+{
+    private Vector3 center;
+    private Vector3 min;
+    private Vector3 max;
+    private Vector3[] pts = new Vector3[8];
+
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        center = transform.position;
+        min = GetComponent<Renderer>().bounds.min;
+        max = GetComponent<Renderer>().bounds.max;
+        pts[0] = min;
+        pts[1] = max;
+        pts[2] = new Vector3(pts[0].x, pts[0].y, pts[1].z);
+        pts[3] = new Vector3(pts[0].x, pts[1].y, pts[0].z);
+        pts[4] = new Vector3(pts[1].x, pts[0].y, pts[0].z);
+        pts[5] = new Vector3(pts[0].x, pts[1].y, pts[1].z);
+        pts[6] = new Vector3(pts[1].x, pts[0].y, pts[1].z);
+        pts[7] = new Vector3(pts[1].x, pts[1].y, pts[0].z);
+    }
+
+    public Vector3[] getPts()
+    {
+        return pts;
+    }
+
+    public Vector3 getMin()
+    {
+        return min;
+    }
+
+    public Vector3 getMax()
+    {
+        return max;
+    }
+
+    public bool intersects(customBoundingBox box)
+    {
+        return (min.x <= box.getMax().x) && (max.x >= box.getMin().x) &&
+                (min.y <= box.getMax().y) && (max.y >= box.getMin().y) &&
+                (min.z <= box.getMax().z) && (max.z >= box.getMin().z);
+    }
+
+    public Vector3 getIntersectPt(customBoundingBox box)
+    {
+        return new Vector3(Mathf.Max(min.x, box.getMin().x), Mathf.Max(min.y, box.getMin().y), Mathf.Max(min.z, box.getMin().z));
+    }
+}
+
---
+++ IFT-2103/Assets/Scripts/tankCollisions.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class tankCollisions : MonoBehaviour {
+    GameObject[] walls;
+    GameObject tankMovement;
+
+    // Use this for initialization
+    void Start () {
+        tankMovement = transform.parent.gameObject;
+        walls = GameObject.FindGameObjectsWithTag("Obstacle");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        customBoundingBox myBox = GetComponent<customBoundingBox>();
+        Vector3 intersectPt;
+        bool isInCollision = false;
+        for (int i = 0; i < walls.Length; i++)
+        {
+            customBoundingBox wallBox = walls[i].GetComponent<customBoundingBox>();
+            if (myBox.intersects(wallBox))
+            {
+                isInCollision = true;
+                intersectPt = myBox.getIntersectPt(wallBox);
+
+                if (isInFront(intersectPt))
+                {
+                    tankMovement.GetComponent<moveTank>().collisionInFront();
+                } else
+                {
+                    tankMovement.GetComponent<moveTank>().collisionInBack();
+                }
+            }
+        }
+
+        if (!isInCollision)
+        {
+            tankMovement.GetComponent<moveTank>().resetCollision();
+        }
+    }
+
+    private bool isInFront(Vector3 _pt)
+    {
+        return Vector3.Dot(transform.forward, _pt) > 0;
+    }
+}

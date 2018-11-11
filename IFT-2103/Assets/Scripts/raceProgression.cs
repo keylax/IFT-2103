56810dc3bb8ed0c31e5a3741d57003a44fb1c4e1
farmville3 <stlaurent_18@hotmail.com>
2018-11-11 01:27:02 -0500

    Progress par until finish line.

---
+++ IFT-2103/Assets/Scripts/raceProgression.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class raceProgression : MonoBehaviour
+{
+
+    public Slider slider;
+    public GameObject finishLine;
+    public GameObject playerCar;
+
+    private Vector3 initialPlayerPosition;
+    private Vector3 finishLinePosition;
+    private float initialDistanceWithFinishLine;
+
+    // Use this for initialization
+    void Start()
+    {
+        initialPlayerPosition =  new Vector3(playerCar.transform.position.x, 0 , playerCar.transform.position.z);
+        finishLinePosition = new Vector3(finishLine.transform.position.x, 0, finishLine.transform.position.z);
+        initialDistanceWithFinishLine = Vector3.Distance(initialPlayerPosition, finishLinePosition);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 currentPlayerPosition = new Vector3(playerCar.transform.position.x, 0, playerCar.transform.position.z);
+        float distanceWithFinishLine = Vector3.Distance(currentPlayerPosition, finishLinePosition);
+        float progressInPercentage = distanceWithFinishLine / initialDistanceWithFinishLine;
+        slider.value = 1 - progressInPercentage;
+    }
+}

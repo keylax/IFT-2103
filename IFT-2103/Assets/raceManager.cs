e2cd14c9b88e13619efb16dee76346f68298ffa6
farmville3 <stlaurent_18@hotmail.com>
2018-11-14 12:08:21 -0500

    Amelioration de la finish line. Amelioration de la map.

---
+++ IFT-2103/Assets/raceManager.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class raceManager : MonoBehaviour {
+
+    public GameObject player;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+
+    public void OnTriggerEnter(Collider trigger)
+    {
+        if(trigger.tag == "Player")
+        {
+            player.GetComponent<carController>().reset();
+        }
+    }
+}

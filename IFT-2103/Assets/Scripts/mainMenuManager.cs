a2cdc1b49d1b48a3bc6835668d2620773a7d4f0f
farmville3 <stlaurent_18@hotmail.com>
2018-11-20 13:33:03 -0500

    Added a MainMenu at the start of the Game.

---
+++ IFT-2103/Assets/Scripts/mainMenuManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class mainMenuManager : MonoBehaviour {
+    public GameObject loadingScreen;
+    public Slider slider;
+    public Text progressText;
+
+    public void LoadLevel(int sceneIndex)
+    {
+        StartCoroutine(LoadAsynchronously(sceneIndex));
+    }
+
+    IEnumerator LoadAsynchronously(int sceneIndex)
+    {
+        yield return null;
+
+        AsyncOperation operation = SceneManager.LoadSceneAsync(sceneIndex);
+
+        loadingScreen.SetActive(true);
+
+        while (!operation.isDone)
+        {
+            new WaitForSeconds(1);
+            float progress = Mathf.Clamp01(operation.progress / .9f);
+
+            progressText.text = progress * 100f + "%";
+            slider.value = progress;
+
+            yield return null;
+        }
+    }
+
+    public void PlayOfflineGame()
+    {
+        LoadLevel(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}

b22ca770d23c218f61ae46bc442b869325654cd6
farmville3 <stlaurent_18@hotmail.com>
2018-12-20 17:51:01 -0500

    Loading screen animation

--- IFT-2103/Assets/Scripts/menuManager.cs
+++ IFT-2103/Assets/Scripts/menuManager.cs
@@ -1,9 +1,16 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class menuManager : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
+    public GameObject loadingScreen;
+    public Slider slider;
+    public Text textPercentage;
+
+    // Use this for initialization
+    void Start () {
 		
 	}
 	
@@ -11,6 +18,31 @@ public class menuManager : MonoBehaviour {
 	void Update () {
 		
 	}
+
+    public void LoadLevel(int sceneIndex)
+    {
+        StartCoroutine(LoadAsynchronously(sceneIndex));
+    }
+
+    IEnumerator LoadAsynchronously(int sceneIndex)
+    {
+        loadingScreen.SetActive(true);
+
+        yield return new WaitForSeconds(2) ;
+
+        AsyncOperation operation = SceneManager.LoadSceneAsync(sceneIndex);
+
+        while (!operation.isDone)
+        {
+            float progress = Mathf.Clamp01(operation.progress / .9f);
+
+            slider.value = progress;
+
+            textPercentage.text = (progress * 100).ToString() + "%"; 
+
+            yield return null;
+        }
+    }
 
     public void QuitGame()
     {

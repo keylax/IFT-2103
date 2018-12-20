ead3d9556a48b76bb88c1a97807b4ead3083d9cb
farmville3 <stlaurent_18@hotmail.com>
2018-12-20 16:27:24 -0500

    Added a main menu

---
+++ IFT-2103/Assets/Scripts/qualityManager.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class qualityManager : MonoBehaviour
+{
+    public Dropdown qualityDropdown;
+
+    public void setQuality()
+    {
+        switch (qualityDropdown.value)
+        {
+            case 0:
+                gameParameters.setQualityLevelToLow();
+                break;
+            case 1:
+                gameParameters.setQualityLevelToNormal();
+                break;
+            case 2:
+                gameParameters.setQualityLevelToHigh();
+                break;
+            default:
+                gameParameters.setQualityLevelToHigh();
+                break;
+        }
+    }
+}
---
+++ IFT-2103/Assets/Scripts/volumeManager.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class volumeManager : MonoBehaviour {
+
+    public Slider volumeSlider;
+
+    public void setVolume()
+    {
+        gameParameters.setVolume((int)(volumeSlider.value * 100));
+    }
+
+}

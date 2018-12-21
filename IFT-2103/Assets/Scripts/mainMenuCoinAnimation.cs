d7937168a968d18cfb3542f0a7b27daae11c6e6d
farmville3 <stlaurent_18@hotmail.com>
2018-12-20 22:42:56 -0500

    Different scenes

---
+++ IFT-2103/Assets/Scripts/mainMenuCoinAnimation.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class mainMenuCoinAnimation : MonoBehaviour {
+
+    public Image coinImage;
+    private Vector3 currentPos;
+
+	// Use this for initialization
+	void Start () {
+        currentPos = coinImage.transform.position;
+        StartCoroutine(anim());
+    }
+	
+	// Update is called once per frame
+	void Update () {
+    }
+
+    IEnumerator MoveToPosition()
+    {
+        yield return null;
+
+        float time = 3f;
+        float elapsedTime = 0f;
+
+        int height = Screen.height;
+        int width = Screen.width;
+
+        float randomX = Random.Range(0, width);
+        float randomY = Random.Range(0, height);
+
+        Vector3 nextPos= new Vector3(randomX, randomY, 0f);
+
+        while (elapsedTime < time)
+        {
+            coinImage.transform.position = Vector3.Lerp(currentPos, nextPos, Ease(elapsedTime / time));
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+
+        coinImage.transform.position = nextPos;
+        currentPos = coinImage.transform.position;
+    }
+
+    IEnumerator anim()
+    {
+        while (true)
+        {
+            StartCoroutine(MoveToPosition());
+            yield return null;
+        }
+    }
+
+    private float Ease(float t)
+    {
+        return t * t * (3f - 2f * t);
+    }
+}

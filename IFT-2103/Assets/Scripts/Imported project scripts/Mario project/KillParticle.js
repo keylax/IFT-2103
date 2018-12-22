fb2e718615a6979b3babc5ce24da390d98ac453e
Guillaume Ouellet <guillaume.ouellet6@ulaval.ca>
2018-12-21 23:46:34 -0500

    Cleanup project (deleted unused downloaded assets

--- IFT-2103/Assets/Scripts/Imported project scripts/Mario project/KillParticle.js
+++
@@ -1,9 +0,0 @@
-/*
-// Kill Particle 
-// Walker Boys (www.walkerboystudio.com)
-// November 28, 2011
-// Description: Deletes this gameObject if called (waits a second)
-// Instruction: Assign script to particle that will be destroyed after 1 second
-*/
-
-Destroy ( this.gameObject, 1.0);
